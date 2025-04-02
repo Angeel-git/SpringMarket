639691a08cda99b83eeaa561438a58d8782b0050
Angel <angelfernandezblanco16@gmail.com>
2025-04-02 16:57:24 +0200

    Creo las interfaces para mapear los objetos de tipo Producto a Product y a la inversa, y los de tipo Categoria a Category y a la invesa, todo esto utilizando MapStruct

---
+++ src/main/java/com/platzi/market/persitence/mapper/CategoryMapper.java
@@ -0,0 +1,24 @@
+package com.platzi.market.persitence.mapper;
+
+import com.platzi.market.domain.Category;
+import com.platzi.market.persitence.entity.Categoria;
+import org.mapstruct.*;
+
+@Mapper(componentModel = "spring") //Indicamos que es un mappeado para convertir
+                                    // las categorias en categories y los productos en products
+public interface CategoryMapper {
+    @Mappings({ // Mapea los campos de la entidad a los campos del DTO
+            @Mapping(source = "idCategoria", target = "categoryId"),
+            @Mapping(source = "descripcion", target = "category"),
+            @Mapping(source = "estado", target = "active")
+
+    })
+    Category toCategory(Categoria categoria); //Convertir categoria a category
+
+    //Indicamos que le conversion es justo la contraria a la de arriba no hace falta volver a poner td los de Mappings
+    @InheritInverseConfiguration
+    //Como en category no tenemos el atributo productos(qeu si tenemos en Categoria) no la vamos a mapear, tenemos que decirle que lo ignore)
+    @Mapping(target = "productos", ignore = true)
+    Categoria toCategoria(Category category);
+
+}
---
+++ src/main/java/com/platzi/market/persitence/mapper/ProductMapper.java
@@ -0,0 +1,41 @@
+package com.platzi.market.persitence.mapper;
+
+import com.platzi.market.domain.Category;
+import com.platzi.market.domain.Product;
+import com.platzi.market.persitence.entity.Producto;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+import java.util.List;
+
+
+//Indicamos que es un mappeado para convertir las categorias en categories y los productos en products
+//El uses = {CategoryMapper.class}, es para que a la hora de mappear la categoria ->category utilice la clase
+// CategoryMapper(donde ya tenemos definidos los mapeadores para esos objetos
+@Mapper(componentModel = "spring", uses = {CategoryMapper.class})
+
+public interface ProductMapper {
+
+    @Mappings({ // Mapea los campos de la entidad a los campos del DTO
+            @Mapping(source = "idProducto", target = "productId"),
+            @Mapping(source = "nombre", target = "name"),
+            @Mapping(source = "idCategoria", target = "categoryId"),
+            @Mapping(source = "precioVenta", target = "price"),
+            @Mapping(source = "cantidadStock", target = "stock"),
+            @Mapping(source = "estado", target = "active"),
+            @Mapping(source = "categoria", target = "category")
+
+
+
+
+
+    })
+    Product toProduct(Producto producto);   //Conversion de producto a product
+    List<Product> toProducts(List<Producto> productos); //conversion de una lista de productos a una lista de products
+
+    @InheritInverseConfiguration
+    @Mapping(target = "codigoBarras", ignore = true)
+    Producto toProducto(Product product);//Conversion de product a producto
+}
