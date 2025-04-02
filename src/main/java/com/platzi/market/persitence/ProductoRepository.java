9b4f3911a006a91ed9707a957e48a8ae20ea85b3
Angel <angelfernandezblanco16@gmail.com>
2025-04-02 17:15:32 +0200

    Enfocamos la clase ProductoRepository hacia el dominio en vez enfocarlo a la tabla

--- src/main/java/com/platzi/market/persitence/ProductoRepository.java
+++ src/main/java/com/platzi/market/persitence/ProductoRepository.java
@@ -1,45 +1,55 @@
 package com.platzi.market.persitence;
 
+import com.platzi.market.domain.Product;
+import com.platzi.market.domain.repository.ProductRepository;
 import com.platzi.market.persitence.crud.ProductoCrudRepository;
 import com.platzi.market.persitence.entity.Producto;
+import com.platzi.market.persitence.mapper.ProductMapper;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository //Indicamops que es una clase que trabaja ocn la bd directamente
-public class ProductoRepository {
+public class ProductoRepository implements ProductRepository {
     private ProductoCrudRepository productoCrudRepository;
+    private ProductMapper mapper;
 
-
-    //--------------------Metodo para recuperar tds los productos de la bd
-    public List<Producto> getAll(){
-        return (List<Producto>) productoCrudRepository.findAll(); //Metodo propio de SpingData en el crudRepository
+    @Override
+    //Recibimos una lista de productos y lo devolvemos como una lista de products
+    public List<Product> getAll(){
+        List<Producto> productos = (List<Producto>) productoCrudRepository.findAll();
+        return mapper.toProducts(productos);
     }
 
-    //-----------------------Metodo para recuperar tds los productos por categoria
-    public List<Producto> getByCategoria(int idCategoria){
-        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    @Override
+    //Recibimos una lista de productos por categoria y lo devolvemos como una lista de products
+    public Optional<List<Product>> getByCategory(int categoryId) {
+        List<Producto> productos = productoCrudRepository.findByIdCategoriaOrderByNombreAsc(categoryId);
+        return Optional.of(mapper.toProducts(productos));
     }
 
-    //Metodo para obtener los productos que se estan agotando(lessThan) y que se estan vendiendo actualmente(estado)
-    public Optional<List<Producto>> getEscasos(int cantidad, boolean estado){
-        return productoCrudRepository.findByCantidadEstockLessThanAndEstado(cantidad, estado);
+    @Override
+    public Optional<List<Product>> getScaseProducts(int quantity) {
+        Optional<List<Producto>> productos = productoCrudRepository.findByCantidadEstockLessThanAndEstado(quantity, true);
+        return productos.map(prods -> mapper.toProducts(prods));
+    }
+
+    @Override
+    public Optional<Product> getProduct(int productId) {
+        return productoCrudRepository.findById(productId).map(producto -> mapper.toProduct(producto));
+    }
+
+    @Override
+    public Product save(Product product) {
+        Producto producto = mapper.toProducto(product);
+        return mapper.toProduct(productoCrudRepository.save(producto));
     }
 
 
-    //Metodo para consultar un producto en particular
-    public Optional<Producto> getProducto(int idProducto){
-        return productoCrudRepository.findById(idProducto);//Metodo propio de SpingData en el crudRepository
-    }
-
-    //Metodo para guardar un producto de mi api
-    public Producto save(Producto producto){
-        return productoCrudRepository.save(producto); //Metodo propio de SpingData en el crudRepository
-    }
-
+    @Override
     //Metodo para eliminar un producto de mi api por el id
-    public  void delete(int idProducto){
-        productoCrudRepository.deleteById(idProducto); //Metodo propio de SpingData en el crudRepository
+    public  void delete(int productId){
+        productoCrudRepository.deleteById(productId); //Metodo propio de SpingData en el crudRepository
     }
 }
