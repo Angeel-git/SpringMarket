4049dccb989c78e730fd047b3d804a589ee6e554
Angel <angelfernandezblanco16@gmail.com>
2025-04-03 10:12:30 +0200

    Uso de ResponseEntity en el controller

--- src/main/java/com/platzi/market/web/controller/ProductController.java
+++ src/main/java/com/platzi/market/web/controller/ProductController.java
@@ -3,6 +3,8 @@ package com.platzi.market.web.controller;
 import com.platzi.market.domain.Product;
 import com.platzi.market.domain.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,31 +19,37 @@ public class ProductController {
 
     //El GetMapping es para mostrar lo que devuelve un metodo, el "/all" es para indicarle la ruta a la vista del metodo en la web
     @GetMapping("/all")
-    public List<Product> getAll(){
-        return productService.getAll();
+    public ResponseEntity<List<Product>> getAll(){
+        return new ResponseEntity<>(productService.getAll(), HttpStatus.OK);
     }
 
     /*En este caso al ruta va entre corchetes ya que se le va a indicar el id del producto desde "PathVariable"
     * Por ejemplo -> http://localhost:8090/platzi-market/api/products/product/4*/
     @GetMapping("/product/{id}")
-    public Optional<Product> getProduct(@PathVariable("id") int productId){
-        return productService.getProduct(productId);
+    public ResponseEntity<Product> getProduct(@PathVariable("id") int productId){
+        return productService.getProduct(productId)
+                .map(product -> new ResponseEntity<>(product, HttpStatus.OK)).orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
 
     @GetMapping("/category/{id}")
-    public Optional<List<Product>> getByCategory(@PathVariable("id") int categoryId){
-        return productService.getByCategory(categoryId);
+    public ResponseEntity<List<Product>> getByCategory(@PathVariable("id") int categoryId){
+        return productService.getByCategory(categoryId)
+                .map(product -> new ResponseEntity<>(product, HttpStatus.OK)).orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     //Como este metodo es para guardar, el llamado es diferente ("PostMapping")
     @PostMapping("/save")
-    public Product save(@RequestBody Product product){  //OJO -> Aqui no es @PathVariable
-        return productService.save(product);
+    public ResponseEntity<Product> save(@RequestBody Product product){  //OJO -> Aqui no es @PathVariable
+        return new ResponseEntity<>(productService.save(product), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/delete/{id}")
-    public boolean delete(@PathVariable("id") int productId){
-        return  productService.delete(productId);
+    public ResponseEntity delete(@PathVariable("id") int productId){
+        if(productService.delete(productId)){
+            return new ResponseEntity((HttpStatus.OK));
+        }else{
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
     }
 }
